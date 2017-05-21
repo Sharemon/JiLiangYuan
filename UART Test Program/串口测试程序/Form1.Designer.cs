544bf55e49b8526da9ddb0a3069f064d7bc9fe4e
sharemon <sharemon@163.com>
2017-05-21 23:15:52 +0800

    add uart local test project

---
+++ UART Test Program/串口测试程序/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace 串口测试程序
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sp = new System.IO.Ports.SerialPort(this.components);
+            this.COM = new System.Windows.Forms.TextBox();
+            this.TxtReceive = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TxtSend = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.TxtCount = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // COM
+            // 
+            this.COM.Location = new System.Drawing.Point(34, 153);
+            this.COM.Margin = new System.Windows.Forms.Padding(2);
+            this.COM.Name = "COM";
+            this.COM.Size = new System.Drawing.Size(39, 21);
+            this.COM.TabIndex = 0;
+            this.COM.Text = "4";
+            // 
+            // TxtReceive
+            // 
+            this.TxtReceive.Location = new System.Drawing.Point(34, 41);
+            this.TxtReceive.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtReceive.Name = "TxtReceive";
+            this.TxtReceive.Size = new System.Drawing.Size(169, 21);
+            this.TxtReceive.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(77, 141);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtSend
+            // 
+            this.TxtSend.Location = new System.Drawing.Point(34, 91);
+            this.TxtSend.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtSend.Name = "TxtSend";
+            this.TxtSend.Size = new System.Drawing.Size(169, 21);
+            this.TxtSend.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 141);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "end";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TxtCount
+            // 
+            this.TxtCount.Location = new System.Drawing.Point(34, 212);
+            this.TxtCount.Name = "TxtCount";
+            this.TxtCount.Size = new System.Drawing.Size(166, 21);
+            this.TxtCount.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 268);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtCount);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtSend);
+            this.Controls.Add(this.TxtReceive);
+            this.Controls.Add(this.COM);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "v";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort sp;
+        private System.Windows.Forms.TextBox COM;
+        private System.Windows.Forms.TextBox TxtReceive;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TxtSend;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox TxtCount;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ UART Test Program/串口测试程序/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 串口测试程序
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sp.BaudRate = 2400;
+            sp.StopBits = System.IO.Ports.StopBits.One;
+            sp.Parity = System.IO.Ports.Parity.None;
+            sp.DataBits = 8;
+            sp.ReadBufferSize = 64;
+            sp.WriteBufferSize = 64;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float[] parameters = new float[7];
+
+
+            this.button1.Enabled = false;
+            sp.PortName = "COM" + COM.Text;
+            sp.Open();
+            while (true)
+            {
+                if (sp.BytesToRead > 5)
+                {
+                    int count = sp.BytesToRead;
+                    TxtCount.Text = count.ToString();
+                    string str = sp.ReadTo(":");
+                    sp.DiscardInBuffer();       //以防“/r/n”堵在readBuffer里面，有数但是读不到“:”从而死机。
+                    TxtReceive.Text = str;
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        if (str[i] == '@')  //找到@作为标志位
+                        {
+                            switch (str[i + 3])
+                            {
+                                case 'R':
+                                    switch (str[i + 4])
+                                    {
+                                        case 'H':
+                                            Random rn = new Random();
+                                            double tempera = 25 + int.Parse(this.textBox1.Text) * rn.NextDouble() / 1000;
+                                            sp.WriteLine("@35RH" + tempera.ToString("0.0000") + ":");
+                                            TxtSend.Text = "@35RH" + tempera.ToString("0.0000") + ":";
+                                            break;
+                                        case 'I':
+                                            Random rn2 = new Random();
+                                            double power = 50 + 20 * (rn2.NextDouble() - 0.5);
+                                            sp.WriteLine("@35RI" + power.ToString("0") + ":");
+                                            break;
+                                        case 'A':
+                                            sp.WriteLine("@35RA" + parameters[0].ToString("0.000") + ":");
+                                            break;
+                                        case 'B':
+                                            sp.WriteLine("@35RB" + parameters[1].ToString("0.000") + ":");
+                                            break;
+                                        case 'C':
+                                            sp.WriteLine("@35RC" + parameters[2].ToString("0.000") + ":");
+                                            break;
+                                        case 'D':
+                                            sp.WriteLine("@35RD" + parameters[3].ToString("0.000") + ":");
+                                            break;
+                                        case 'E':
+                                            sp.WriteLine("@35RE" + parameters[4].ToString("0.000") + ":");
+                                            break;
+                                        case 'F':
+                                            sp.WriteLine("@35RF" + parameters[5].ToString("0.000") + ":");
+                                            break;
+                                        case 'G':
+                                            sp.WriteLine("@35RG" + parameters[6].ToString("0.000") + ":");
+                                            break;
+                                        default: MessageBox.Show(str); break;
+                                    }
+                                    break;
+                                case 'W':
+                                    float value = float.Parse(str.Remove(0, 5));
+                                    parameters[str[i + 4] - 'A'] = value;
+                                    sp.WriteLine(str.Substring(0, 5) + ":");
+                                    TxtSend.Text = parameters[str[i + 4] - 'A'].ToString("0.000");
+                                    break;
+                                default: MessageBox.Show(str); break;
+                            }
+                        }
+                    }
+                }
+                Application.DoEvents();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sp.Close();
+        }
+    }
+}
